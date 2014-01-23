999e753b9910f46768a464d7061b36912ad7e74f
Birk Nilson <birk@tictail.com>
2014-01-23 01:51:14 +0100

    Implemented storefront snippet

--- berghs.py
+++ berghs.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from flask import Flask, json, render_template, request as flask_request
+from datetime import timedelta
+from functools import update_wrapper
+from flask import (
+    Flask, json, render_template, make_response,
+    current_app, request as flask_request,
+)
 
 app = Flask(__name__)
 
@@ -10,6 +15,48 @@ db = {
     'products': {},
     'hashtags': {}
 }
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and flask_request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and flask_request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 def get_cached_hashtag(product_id, default=None):
     return db['hashtags'].get(product_id, default)
@@ -83,6 +130,7 @@ def get_teapot():
 
 
 @app.route('/api/v1/stores/<store_id>/hashtags', methods=['GET', 'PUT'])
+@crossdomain(origin='*')
 def get_or_update_hashtags(store_id):
     access_token = flask_request.args.get('access_token')
     if flask_request.method == 'GET':
---
+++ static/js/snippet.js
@@ -0,0 +1,115 @@
+(function(TT, Settings) {
+  var Berghsnippet = function(settings) {
+    // Instagram token this time around
+    this.settings = settings;
+    this.hashtags = {};
+    if (this.isProductPage()) {
+      var self = this;
+      this.getHashtags(function(hashtags) {
+        self.getInstagrams(function(hashtag, instagrams) {
+          self.renderList(hashtag, instagrams);
+        });
+      });
+    }
+  };
+
+  Berghsnippet.prototype = {
+    isProductPage: function() {
+      return this.settings.productIdentifier.length;
+    },
+
+    getHashtags: function(callback) {
+      var self = this;
+      var url = this.settings.api + '/stores/' + this.settings.storeIdentifier + '/hashtags';
+      $.get(url, function(hashtags) {
+        var current, count = hashtags.length;
+        for (var i = 0; i < count; i++) {
+          current = hashtags[i];
+          self.hashtags[current.product_id] = current.hashtag;
+        }
+        callback(self.hashtags);
+      }, 'json');
+    },
+
+    getInstagrams: function(callback) {
+      var hashtag = this.hashtags[this.settings.productIdentifier];
+      if (!hashtag) {
+        alert('No hashtag found for product :-( Something is wrong');
+        return;
+      }
+
+      hashtag = 'tictail';
+      var self = this;
+      $.ajax({
+        url: 'https://api.instagram.com/v1/tags/' + hashtag + '/media/recent?access_token=' + this.settings.accessToken,
+        jsonp: 'callback',
+        dataType: 'jsonp',
+        success: function(response) {
+          callback(hashtag, self.prepareInstagrams(response));
+        }
+      });
+    },
+
+    prepareInstagrams: function(response) {
+      var data, count, i, current, ret = [];
+      data = response.data;
+      count = data.length;
+
+      for (i = 0; i < count; i++) {
+        current = data[i];
+        ret.push({
+          'image': current.images.low_resolution.url,
+          'caption': current.caption.text,
+          'link': current.link
+        });
+      }
+      return ret;
+    },
+
+    renderList: function(hashtag, instagrams) {
+      var count = instagrams.length;
+      if (!count) {
+        return;
+      }
+
+      var container, i;
+      container = $('<ul class="instagrams">');
+      container.append('<li><h2>#' + hashtag + '</h2></li>');
+      for (i = 0; i < count; i++) {
+        container.append(this.renderItem(instagrams[i]));
+      }
+      $('.tictail_add_to_cart').before(container);
+    },
+
+    renderItem: function(instagram) {
+      var container, link, image;
+      container = $('<li>');
+      link = $('<a>');
+      image = $('<img>');
+
+      link.attr('href', instagram.link);
+      image.attr('src', instagram.image);
+      image.attr('alt', instagram.caption);
+
+      link.append(image);
+      container.append(link);
+      return container;
+    }
+  };
+
+  var initSnippet = function() {
+    new Berghsnippet(Settings);
+  };
+
+  if (!Settings.accessToken) {
+    throw Error('No Instagram access_token given. Cannot proceed.');
+    alert('Your instagram access_token has not been setup.');
+  }
+
+  // Checks if jQuery is present on the page. 
+  if (TT.hasjQuery && !TT.hasjQuery()) {
+    TT.loadScript('tt-myapp-jq', 'http://code.jquery.com/jquery-1.10.2.js', initSnippet);
+  } else {
+    initSnippet();
+  }
+})(window.TT || {}, window.Berghsnippet || {});
